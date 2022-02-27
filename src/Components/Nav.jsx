5a67f9faeaa8c74258101e3fe9849d30ee70b995
alexalextran <alextran8385@gmail.com>
2022-02-27 21:56:49 +1100

    navbar styled

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Search, Bell, PersonSquare, CaretDown } from 'react-bootstrap-icons';
+import netflix from '../assets/netflix.svg'
 
 const Nav = () => {
-    return (
-        <nav>
-            NAVBAR!!!!!!!!!!
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <div className="navigation__links">
+        <img src={netflix}></img>
+        <ul>
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>New&Popular</li>
+          <li>My List</li>
+        </ul>
+
+      </div>
+
+      <div className="nav__right">
+        <Search />
+       
+        <a>Kids</a> 
+        <Bell  />
+        <div>
+            <PersonSquare  /> 
+            <CaretDown  />
+        </div>
+       </div>
+    
+ 
+    
+
+    </nav>
+  );
+};
 
 export default Nav;
