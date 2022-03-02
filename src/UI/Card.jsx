d6557cf301bbeca0dbc19d5af2ace11949bd41c2
alexalextran <alextran8385@gmail.com>
2022-03-02 21:32:44 +1100

    css styling and stuffs

--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -1,6 +1,12 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { PlayCircleFill, PlusCircle, HandThumbsUp, HandThumbsDown } from 'react-bootstrap-icons';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {
+  PlayCircleFill,
+  PlusCircle,
+  HandThumbsUp,
+  HandThumbsDown,
+ ArrowDownCircle,
+} from "react-bootstrap-icons";
 
 const Card = (props) => {
   const [data, setdata] = useState();
@@ -9,79 +15,75 @@ const Card = (props) => {
   const [loaded, setloaded] = useState(false);
 
   const getDetails = async () => {
-    
-    try{
+    try {
+      var hehehaw = await axios.get(
+        `https://api.themoviedb.org/3/movie/${props.id}?api_key=96b4f7f5b36d1f202a75b5f548cf311e&append_to_response=images&include_image_language=en`
+      );
+    } catch (error) {
+      var hehehaw = await axios.get(
+        `https://api.themoviedb.org/3/tv/${props.id}?api_key=96b4f7f5b36d1f202a75b5f548cf311e&append_to_response=images&include_image_language=en`
+      );
+    }
 
-    
-      var hehehaw = await axios.get(`https://api.themoviedb.org/3/movie/${props.id}?api_key=96b4f7f5b36d1f202a75b5f548cf311e&append_to_response=images&include_image_language=en`)
-  } catch (error){
-    var hehehaw = await axios.get(`https://api.themoviedb.org/3/tv/${props.id}?api_key=96b4f7f5b36d1f202a75b5f548cf311e&append_to_response=images&include_image_language=en`)
+    setdata(hehehaw.data);
+    setloaded(true);
 
+    if (data.images.backdrops.length !== 0) {
+      setimg(data.images.backdrops[0].file_path);
+    } else {
+      setimg(props.img);
+    }
 
-  }
-
- 
-
-  setdata(hehehaw.data)
-    setloaded(true)
-
-
-if(data.images.backdrops.length !== 0){
-  setimg(data.images.backdrops[0].file_path)
- 
-}
-else{
-  setimg(props.img)
-}
-
-
-
-  if(data.original_name == undefined || null){
-    setname(data.title)
-  } else{
- 
-    setname(data.name)
-  }
-}
-
-
-
-
-
+    if (data.original_name == undefined || null) {
+      setname(data.title);
+    } else {
+      setname(data.name);
+    }
+  };
 
   useEffect(() => {
-    getDetails()
-   
+    getDetails();
   }, [loaded]);
 
- 
-  
-    return (
-      <>
-        <div className='card'>
-          <img src={`https://image.tmdb.org/t/p/w500${props.img}`}></img> 
-          
-            <div className='card__detailed'>
-    <div className='card__detailed--top'>
-       {loaded ? <img className='card__detailed--image' src={`https://image.tmdb.org/t/p/w500${img}`}></img> : "loading"}
-    </div>
-    <div>
-    <div className=''> 
-      <PlayCircleFill />
-      <PlusCircle />
-      <HandThumbsUp />
-      <HandThumbsDown />
-    </div>
-      {loaded ? <p>{name}</p> : "loading"}
-    </div>
-    
-       
-        </div>
-        </div>
+  return (
+    <>
+      <div className="card">
+        <img src={`https://image.tmdb.org/t/p/w500${props.img}`}></img>
 
-       
-        </>
-    );
-}
+        <div className="card__detailed">
+          <div className="card__detailed--top">
+            {loaded ? (
+              <img
+                className="card__detailed--image"
+                src={`https://image.tmdb.org/t/p/w500${img}`}
+              ></img>
+            ) : (
+              "loading"
+            )}
+          </div>
+
+          <div className="card__detailed--bottom">
+            <div className="card__detailed--icons">
+            <div>
+              <PlayCircleFill />
+              <PlusCircle />
+              <HandThumbsUp />
+              <HandThumbsDown />
+            </div>
+            <div>
+            <ArrowDownCircle />
+            </div>
+              
+            </div>
+            <div>
+              {loaded ? <p>{name}</p> : "loading"}
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Card;
