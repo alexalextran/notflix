fbf41bb68beeaf7d6b91349b389772e26478b41b
alexalextran <alextran8385@gmail.com>
2022-03-07 00:00:17 +1100

    base for carousel

--- src/UI/CategoryWrapper.jsx
+++ src/UI/CategoryWrapper.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {
+    ChevronRight
+  } from "react-bootstrap-icons";
 
 const Categorywrapper = (props) => {
     return (
         <section className='trending'>
         <h1>{props.title}</h1>
+
+        <ChevronRight className='arrowRight'/>
         <div className='card__wrapper'>
 
         
