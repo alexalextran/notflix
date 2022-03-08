4f6ab3c36adee5b393044a6ec9692f155e46e7fd
alexalextran <alextran8385@gmail.com>
2022-03-09 01:15:23 +1100

    basic carousel setup

--- src/UI/CategoryWrapper.jsx
+++ src/UI/CategoryWrapper.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
     ChevronRight
   } from "react-bootstrap-icons";
 
 const Categorywrapper = (props) => {
+
+const arrowRef = useRef();
+var right = 0
+const carousel = (e) => {
+    
+    if(e.target === arrowRef.current){
+        console.log(arrowRef.current.parentElement)
+        right += 80
+        console.log(right)
+        arrowRef.current.parentElement.style.transform = `translateX(-${right}vw)`
+    }
+}
+
+
+
+ 
+
+
     return (
         <section className='trending'>
         <h1>{props.title}</h1>
 
-        <ChevronRight className='arrowRight'/>
+        
         <div className='card__wrapper'>
 
         
         <div className='tending__cards'>
+        <ChevronRight className='arrowRight' ref={arrowRef} onClick={carousel} />
             { 
                 props.loadingstate
             }
