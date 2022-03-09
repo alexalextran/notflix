85df12aa864979f9d190f52294371a167c99da9c
alexalextran <alextran8385@gmail.com>
2022-03-09 23:05:50 +1100

    switched usestate to useref so it stopped bugging out

--- src/UI/CategoryWrapper.jsx
+++ src/UI/CategoryWrapper.jsx
@@ -1,52 +1,66 @@
-import React, { useRef, useState } from 'react';
-import {
-    ChevronRight
-  } from "react-bootstrap-icons";
+import React, { useRef, useState } from "react";
+import { ChevronLeft, ChevronRight } from "react-bootstrap-icons";
 
 const Categorywrapper = (props) => {
+  const leftarrowRef = useRef();
+  const rightarrowRef = useRef();
+  const reference = useRef(0);
 
-const arrowRef = useRef();
-const [right, setright] = useState(100);
+  const carousel = (e) => {
+    if (e.target === rightarrowRef.current) {
+      var html = rightarrowRef.current.parentElement.children[2].children;
+      const value = reference.current + 100;
 
-const carousel = (e) => {
-    
-    if(e.target === arrowRef.current){
-        // console.log(arrowRef.current.parentElement.children[1].children)
-        setright(right + 100)
+      for (let i = 0; i < html.length; i++) {
+        html[i].style.transform = `translateX(-${value}vw)`;
+      }
 
-       
-        var html = arrowRef.current.parentElement.children[1].children
-        console.log(right)
-    }   
+      reference.current = value;
+    } else {
+      var html = leftarrowRef.current.parentElement.children[2].children;
+      const value = reference.current - 100;
 
-    for (let i = 0; i < html.length; i++) {
-            html[i].style.transform = `translateX(-${right}vw)`
-          }
-}
+      for (let i = 0; i < html.length; i++) {
+        html[i].style.transform = `translateX(-${value}vw)`;
+      }
 
+      reference.current = value;
+    }
 
+    if (reference.current >= 300) {
+      rightarrowRef.current.style.display = "none";
+    } else {
+        rightarrowRef.current.style.display = "block";
+    }
 
- 
+    if (reference.current >= 100) {
+        leftarrowRef.current.style.display = "block";
+      } else {
+        leftarrowRef.current.style.display = "none";
+      }
+  };
 
+  return (
+    <section className="trending">
+      <h1>{props.title}</h1>
 
-    return (
-        <section className='trending'>
-        <h1>{props.title}</h1>
+      <div className="card__wrapper">
+        <ChevronRight
+          className="arrowRight"
+          ref={rightarrowRef}
+          onClick={carousel}
+        />
 
-    
-       
-        <div className='card__wrapper'>
-        <ChevronRight className='arrowRight' ref={arrowRef} onClick={carousel} />
-        
-        <div className='tending__cards'>
-        
-            { 
-                props.loadingstate
-            }
-        </div>
-            </div> 
-        </section>
-    );
-}
+        <ChevronLeft
+          className="arrowLeft"
+          ref={leftarrowRef}
+          onClick={carousel}
+        />
+
+        <div className="tending__cards">{props.loadingstate}</div>
+      </div>
+    </section>
+  );
+};
 
 export default Categorywrapper;
