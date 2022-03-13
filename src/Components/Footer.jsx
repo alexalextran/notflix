6c4c8a35e18f94af430cc1f02ee9e9e3df2853ae
alexalextran <alextran8385@gmail.com>
2022-03-13 22:03:33 +1100

    simple footer design

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+     © Copyright Some fake company
+      <div>
+        <ul>
+          <li>footer link</li>
+          <li>footer link</li>
+          <li>link link</li>
+          <li>footer footer</li>
+          <li>link link</li>
+        </ul>
+        <ul>
+          <li>link other</li>
+          <li>link other</li>
+          <li>other other</li>
+          <li>other other</li>
+          <li>link other</li>
+        </ul>
+        <ul>
+          <li>some link</li>
+          <li>some link</li>
+          <li>footer link</li>
+          <li>link link</li>
+          <li>footer some</li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -14,6 +14,7 @@ import {
 } from "react-bootstrap-icons";
 import Loading from "../UI/Loading";
 import Nav from "../Components/Nav";
+import Footer from "../Components/Footer";
 
 
 const Homepage = () => {
@@ -139,6 +140,7 @@ const Homepage = () => {
           search={"16"}
         />
       </div>
+      <Footer />
     </>
   );
 };
