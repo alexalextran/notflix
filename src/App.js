75460da99f75ed73e60cb8fd9c89d9719cb3a093
alexalextran <alextran8385@gmail.com>
2022-03-15 20:51:46 +1100

    i think i fixed the 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./SASS/App.css";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import Homepage from "./Pages/HomePage";
 import SignUpPage from "./Pages/SignUpPage";
 import { AuthProvider } from "./Contexts/AuthContext";
@@ -11,23 +16,21 @@ import Fourofour from "./Pages/fourOfour";
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <div className="App">
-      <AuthProvider>
-        <Routes basename={process.env.PUBLIC_URL}> 
-          <Route path="/signup" exact element={<SignUpPage />} />
+      <div className="App">
+        <AuthProvider>
+          <Routes basename={process.env.PUBLIC_URL}>
+            <Route path="/signup" exact element={<SignUpPage />} />
 
-          <Route path="/login" exact element={<Loginpage />} />
+            <Route path="/login" exact element={<Loginpage />} />
 
-          <Route path="*" exact element={<Fourofour />} />
-
-          <Route exact path="/" element={<PrivateRoute />}>
-            <Route exact path="/" element={<Homepage />} />
-          </Route>
-          
-        </Routes>
-      </AuthProvider>
-    </div>
-   </BrowserRouter>
+            <Route exact path="/" element={<PrivateRoute />}>
+              <Route exact path="/" element={<Homepage />} />
+              <Route path="*" element={<Fourofour />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </div>
+    </BrowserRouter>
   );
 }
 
